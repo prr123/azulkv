932f42f0782c7f2bd1f40e38fb0a2290b3ad5957
Peter Riemenschneider <azulsoftwarevlc@gmail.com>
2023-08-28 16:55:53 +0200

    add load and backup

--- azulkvTest.go
+++ azulkvTest.go
@@ -3,22 +3,37 @@ package main
 
 import (
 	"fmt"
-//	"log"
+	"log"
 //	"os"
 	"db/azulkv/azulkvLib"
 )
 
 func main() {
 
-	kvMap, err := azulkv.InitKV()
-	if err != nil {panic(err)}
+	kvMap, err := azulkv.InitKV("testDb", true)
+	if err != nil {log.Fatalf("error -- InitKV: %v", err)}
 
 //	fmt.Printf("kvmap actual %d capacity: %d\n", kvMap.NumEntries, kvMap.Cap)
 //	fmt.Printf("hash len: %d\n", len(*kvMap.Hash))
 
+
 	kvMap.FillRan(5)
 
+	azulkv.PrintDb(kvMap)
 	kvMap.PrintKV(0,5)
+
+	log.Printf("create backup!")
+	err = kvMap.Backup("tabBackup.dat")
+	if err != nil {log.Fatalf("error -- Backup: %v", err)}
+
+	kvdb, err := azulkv.InitKV("testDb", true)
+	if err != nil {log.Fatalf("error -- InitKV: %v", err)}
+	log.Printf("load Backup!")
+	err = kvdb.Load("tabBackup.dat")
+	if err != nil {log.Fatalf("error -- Load: %v", err)}
+
+	azulkv.PrintDb(kvdb)
+	kvdb.PrintKV(0,5)
 
 	fmt.Printf("success\n")
 
